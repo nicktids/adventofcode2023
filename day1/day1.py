a7ac7c19317dfeb47b057b405db6827f245a456a
nicktids <nicktids@gmail.com>
2023-12-04 14:11:56 +0800

    day 1 complete

---
+++ day1/day1.py
@@ -0,0 +1,27 @@
+#%%
+import re
+#%%
+# Opening file
+file1 = open('input.txt', 'r')
+count = 0
+ #%%
+ 
+total = 0
+# Using for loop
+file1 = open('input.txt', 'r')
+count = 0
+print("Using for loop")
+for line in file1:
+    count += 1
+    x = re.findall(r'\d', line)
+    # re.match('^\D*\d\D*$', item)
+    first = int(x[0]) *10
+    last = int(x[-1])
+    addition = first + last
+    total += (first + last)
+    print("Line{}: {}".format(count, line.strip()))
+    print(addition)
+print(total)
+# Closing files
+file1.close()
+# %%
